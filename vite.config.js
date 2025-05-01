672e32bbd511d1aefe23a7ab0e7bdfb5d72fb598
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2025-04-30 21:10:12 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Ajusta esto según tu ruta base de despliegue
+  base: './', // Ajusta esto según tu ruta base de despliegue
   plugins: [
     vue(),
   ],
